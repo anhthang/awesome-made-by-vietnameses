72162e593fbb9a3ac17a0c001444dc2c9caeba4c
Anh Thang Bui <buianhthang89@gmail.com>
2020-10-08 18:04:55 +0700

    Update responsive

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { onMount } from 'svelte';
-	import Repo from './Repo.svelte';
 	import awesome from './awesome';
+
+	import Repo from './Repo.svelte';
+	import ViewOnGithub from './ViewOnGithub.svelte';
 
 	export let name;
 
@@ -16,16 +18,14 @@
 </script>
 
 <main>
-	<h1>{name}</h1>
-	<p>
-		It's hard to calculate project popularity so we use stars as a measurement. It's not quite fair, but it is what we have here on Github.
-	</p>
+	<ViewOnGithub />
 
-	<a id="github-repo" href="https://github.com/anhthang/awesome-made-by-vietnameses" target="_blank">
-		<svg width="48" height="48" fill="#fcd838" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Fork me on GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
-	</a>
+	<h1>{name}</h1>
 
 	<div class="categories">
+		<p>
+			It's hard to calculate project popularity so we use stars as a measurement. It's not quite fair, but it is what we have here on Github.
+		</p>
 		{#each categories as category}
 			<span style="color: {colors[category]?.color}">{category}</span>
 			<div class="repos">
@@ -41,26 +41,15 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		/* max-width: 240px; */
 		margin: 0 auto;
-	}
-
-	.categories {
-		width: 70%;
-		margin-left: 15%;
 	}
 
 	h1 {
 		color: #ff3e00;
 		/* text-transform: uppercase; */
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 
 	.categories span {
@@ -72,31 +61,33 @@
 	.repos {
 		width: 100%;
 		display: grid;
-		grid-template-columns: repeat(3, 1fr);
+		grid-template-columns: repeat(1, 1fr);
 		grid-gap: 8px;
 
 		margin-top: 16px;
 		margin-bottom: 16px;
 	}
 
-	#github-repo {
-		position: fixed;
-		top: 40px;
-		right: 30px;
-		z-index: 99;
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
 	}
 
-	@media (prefers-reduced-motion: no-preference) {
-		#github-repo {
-			animation: github-repo-spin infinite 10s linear;
+	@media (min-width: 768px) {
+		.repos {
+			grid-template-columns: repeat(2, 1fr);
 		}
 	}
-	@keyframes github-repo-spin {
-		from {
-			transform: rotate(0deg);
+
+	@media (min-width: 1200px) {
+		.categories {
+			width: 70%;
+			margin-left: 15%;
 		}
-		to {
-			transform: rotate(360deg);
+
+		.repos {
+			grid-template-columns: repeat(3, 1fr);
 		}
 	}
 </style>
